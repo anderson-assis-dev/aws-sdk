e39edca971299ef027e762b795a076723c9537c8
Anderson Nascimento <marinalva.assisnogueira@gmail.com>
2024-05-27 20:58:28 -0300

    versionamento inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+import {CreateBucketCommand, S3Client} from '@aws-sdk/client-s3'
+
+async function main(){
+    const region = 'us-east-2'
+    const bucketName = `hcode-treinamentos-${Date.now()}`;
+    const s3 = new S3Client({
+        region
+    })
+    await s3.send(new CreateBucketCommand({
+        Bucket: bucketName
+    }))
+    console.log(s3)
+}
+
+
